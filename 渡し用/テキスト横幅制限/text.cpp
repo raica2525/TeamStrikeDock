a8c8ec246acbfee17015f4f14d268e993062a8cc
raica2525 <jb2019039@stu.yoshida-g.ac.jp>
2021-08-27 09:07:45 +0900

    テキスト横幅制限追加

---
+++ 渡し用/テキスト横幅制限/text.cpp
@@ -0,0 +1,157 @@
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+//
+//	text.cpp
+//	Author:池田悠希
+//
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+//インクルードファイル
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+#include "text.h"
+#include "manager.h"
+#include "renderer.h"
+
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+//コンストラクタ
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+CText::CText() :CScene(OBJTYPE::OBJTYPE_TEXT)
+{
+    m_pFont = NULL;
+    m_pos = DEFAULT_VECTOR;
+    m_nSize = 0;
+	m_nLimWidth = 0;
+    m_col = 0xffffffff;
+    m_align = ALIGN_LEFT;
+    m_format = NULL;
+    ZeroMemory(m_fontName, sizeof(m_fontName));
+    ZeroMemory(m_str, sizeof(m_str));
+}
+
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+//デストラクタ
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+CText::~CText()
+{
+
+}
+
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+//初期化処理
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+HRESULT CText::Init(D3DXVECTOR3 pos, D3DXVECTOR3 size)
+{
+	int nWidth = 0;
+	if (m_nLimWidth < strlen(m_str)*m_nSize || m_nLimWidth == 0)
+	{
+		nWidth = m_nLimWidth / strlen(m_str);
+	}
+	else
+	{
+		nWidth = m_nSize;
+	}
+	//フォント生成
+	D3DXCreateFont(CManager::GetRenderer()->GetDevice(),
+		m_nSize,
+		nWidth,
+		0,
+		0,
+		FALSE,
+		SHIFTJIS_CHARSET,
+		OUT_DEFAULT_PRECIS,
+		DEFAULT_QUALITY,
+		DEFAULT_PITCH,
+		m_fontName,
+		&m_pFont);
+    switch (m_align)
+    {
+    case ALIGN_LEFT:
+        m_format = DT_LEFT;
+        break;
+    case ALIGN_CENTER:
+        m_format = DT_CENTER;
+        break;
+    case ALIGN_RIGHT:
+        m_format = DT_RIGHT;
+        break;
+    default:
+        break;
+    }
+    return S_OK;
+}
+
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+//終了処理
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+void CText::Uninit(void)
+{
+    if (m_pFont != NULL)
+    {
+        m_pFont->Release();
+        m_pFont = NULL;
+    }
+
+    // オブジェクトの破棄
+    Release();
+}
+
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+//更新処理
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+void CText::Update(void)
+{
+
+}
+
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+//描画処理
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+void CText::Draw(void)
+{
+    RECT rect;
+    SetRect(&rect, 0, 0, 0, 0);
+    m_pFont->DrawText(NULL, m_str, -1, &rect, DT_CALCRECT | m_format, m_col);
+    OffsetRect(&rect, (int)m_pos.x, (int)m_pos.y);
+    m_pFont->DrawText(NULL, m_str, -1, &rect, m_format, m_col);
+}
+
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+//生成処理
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+CText* CText::Create(D3DXVECTOR3 pos, int nSize, char* pStr, ALIGN align, char* pFontName, D3DCOLOR col, int nLimWidth)
+{
+    CText* pText = new CText;
+    pText->m_pos = pos;
+    pText->m_nSize = nSize;
+    wsprintf(pText->m_str, pStr);
+    pText->m_align = align;
+    wsprintf(pText->m_fontName, pFontName);
+    pText->m_col = col;
+	pText->m_nLimWidth = nLimWidth;
+    pText->Init(DEFAULT_VECTOR, DEFAULT_VECTOR);
+    return pText;
+}
+
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+//フォントファイルの読み込み
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+void CText::Load(void)
+{
+    AddFontResourceEx("data\\FONT\\NikkyouSans-mLKax.ttf", FR_PRIVATE, 0);
+}
+
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+//フォントデータの破棄
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+void CText::Unload(void)
+{
+    RemoveFontResourceEx("data\\FONT\\NikkyouSans-mLKax.ttf", FR_PRIVATE, 0);
+}
+
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+//テキスト変更関数
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+void CText::SetText(char * pStr)
+{
+    wsprintf(m_str, pStr);
+}
---
+++ 渡し用/テキスト横幅制限/text.h
@@ -0,0 +1,53 @@
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+//
+//	text.h
+//	Author:池田悠希
+//
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+#ifndef _TEXT_H_
+#define _TEXT_H_
+
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+//インクルードファイル
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+#include "main.h"
+#include "scene.h"
+
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+//クラス定義
+//・・・・・・・・・・・・・・・・・・・・・・・・・・・
+class CText : public CScene
+{
+public:
+	//列挙型宣言
+    typedef enum
+    {
+        ALIGN_LEFT = 0,
+        ALIGN_CENTER,
+        ALIGN_RIGHT
+    }ALIGN;
+
+    CText();	//コンストラクタ
+    ~CText();	//デストラクタ
+    HRESULT Init(D3DXVECTOR3 pos, D3DXVECTOR3 size);	//初期化処理
+    void Uninit(void);	//終了処理
+    void Update(void);	//更新処理
+    void Draw(void);	//描画処理
+    static CText* Create(D3DXVECTOR3 pos, int nSize, char* pStr, ALIGN align, char* pFontName, D3DCOLOR col,int nLimWidth = 0);	//生成処理
+    static void Load(void);	//フォントファイルの読み込み
+    static void Unload(void);	//フォントデータの破棄
+
+    void SetText(char* pStr);	//テキスト変更関数
+
+private:
+    LPD3DXFONT m_pFont;	//フォント保存用変数
+    D3DXVECTOR3 m_pos;	//座標
+    int m_nSize;	//文字のサイズ
+	int m_nLimWidth;	//文字列の幅の上限
+    D3DCOLOR m_col;	//色
+    ALIGN m_align;	//文字の整列の方法
+    DWORD m_format;	//整列方法変換用変数
+    char m_fontName[128];	//フォントの名前
+    char m_str[512];	//表示する文字列
+};
+#endif
